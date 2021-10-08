df060f32a80e7450a56c4881bf4db4493d0ed860
Andrea Rizzi <andrrizzi@gmail.com>
2021-10-08 08:49:47 +0200

    nn subpackage imports

--- tfep/nn/flows/__init__.py
+++ tfep/nn/flows/__init__.py
@@ -8,3 +8,7 @@ method which backpropagate the gradients).
 The forward propagation of the modules here return both the transformation
 of the input plus the log determinant of the Jacobian.
 """
+
+from tfep.nn.flows.maf import MAF
+from tfep.nn.flows.partial import PartialFlow
+from tfep.nn.flows.sequential import SequentialFlow
--- tfep/nn/transformers/__init__.py
+++ tfep/nn/transformers/__init__.py
@@ -8,3 +8,12 @@ method which backpropagate the gradients).
 The forward propagation of the modules here return both the transformation
 of the input plus the log determinant of the Jacobian.
 """
+
+from tfep.nn.transformers.affine import (
+    AffineTransformer, affine_transformer, affine_transformer_inverse)
+from tfep.nn.transformers.mobius import (
+    MobiusTransformer, MobiusTransformerFunc, mobius_transformer, unit_cube_to_inscribed_sphere)
+from tfep.nn.transformers.sos import (
+    SOSPolynomialTransformer, SOSPolynomialTransformerFunc, sos_polynomial_transformer)
+from tfep.nn.transformers.spline import (
+    NeuralSplineTransformer, neural_spline_transformer)
