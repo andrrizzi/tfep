394606796657a6bacd9e527149089aec5c7c76fb
Andrea Rizzi <andrrizzi@gmail.com>
2021-10-17 22:56:58 +0200

    CenteredCentroidFlow

---
+++ tfep/tests/nn/flows/test_centroid.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+
+
+# =============================================================================
+# MODULE DOCSTRING
+# =============================================================================
+
+"""
+Test objects and function in the module tfep.nn.flow.centroid.
+"""
+
+
+# =============================================================================
+# GLOBAL IMPORTS
+# =============================================================================
+
+import numpy as np
+import pytest
+import torch
+
+from tfep.nn.flows.centroid import CenteredCentroidFlow
+from tfep.utils.geometry import atom_to_flattened, flattened_to_atom, atom_to_flattened_indices
+
+# =============================================================================
+# GLOBAL VARIABLES
+# =============================================================================
+
+# Random number generator. Makes sure tests are reproducible from run to run.
+GENERATOR = torch.Generator()
+GENERATOR.manual_seed(0)
+
+
+# =============================================================================
+# UTILITY FUNCTIONS
+# =============================================================================
+
+class IdentityFlow:
+
+    def __call__(self, x):
+        return x, torch.zeros(len(x))
+
+    def inverse(self, y):
+        return self(y)
+
+
+class TranslateFlow:
+
+    def __call__(self, x, sign=1):
+        n_dofs = x.shape[-1]
+        translate = torch.arange(n_dofs, dtype=x.dtype)
+        return x + sign*translate, torch.zeros(len(x))
+
+    def inverse(self, y):
+        return self(y, sign=-1)
+
+
+# =============================================================================
+# TESTS
+# =============================================================================
+
+def test_inconsistent_configuration():
+    """Test that subset_point_indices and weights must have the same length."""
+    space_dimension = 3
+    with pytest.raises(ValueError, match='length'):
+        CenteredCentroidFlow(
+            None,  # flow, not needed.
+            space_dimension,
+            subset_point_indices=[2, 3, 4],
+            weights=[4, 6]
+        )
+
+
+@pytest.mark.parametrize('exclude_fixed_point', [False, True])
+@pytest.mark.parametrize('subset_point_indices,weights', [
+    (None, None),
+    ([0, 1], None),
+    ([1, 2], torch.tensor([0.25, 0.75])),
+])
+@pytest.mark.parametrize('fixed_point_idx', [0, 1])
+def test_compute_centroid(exclude_fixed_point, subset_point_indices, weights, fixed_point_idx):
+    """Test CenteredCentroidFlow._compute_centroid()."""
+    coords = torch.tensor([
+        [[0, 0, 0], [1, 1, 1], [2, 2, 2]],
+        [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+    ], dtype=torch.float64)
+
+    # Initialize flow.
+    flow = CenteredCentroidFlow(
+        None,  # Flow. Not needed here.
+        space_dimension=coords.shape[-1],
+        subset_point_indices=subset_point_indices,
+        weights=weights,
+        fixed_point_idx=fixed_point_idx,
+    )
+
+    # Compute the centroid.
+    centroid = flow._compute_centroid(coords, exclude_fixed_point)
+    if exclude_fixed_point:
+        centroid, weight = centroid
+
+    # Compute the expected centroid with numpy.
+    coords = coords.detach().numpy()
+    if subset_point_indices is not None:
+        coords = coords[:, subset_point_indices]
+
+    if weights is not None:
+        weights = weights.detach().numpy()
+
+    expected_centroid = np.average(coords, axis=1, weights=weights)
+    if exclude_fixed_point:
+        w = 1/coords.shape[1] if weights is None else weights[fixed_point_idx]
+        assert w == weight
+        expected_centroid = expected_centroid - coords[:, fixed_point_idx]*w
+
+    assert np.allclose(centroid, expected_centroid)
+
+
+@pytest.mark.parametrize('flow', [IdentityFlow, TranslateFlow])
+@pytest.mark.parametrize('space_dimension', [1, 2, 3])
+@pytest.mark.parametrize('subset_point_indices,weights', [
+    (None, None),
+    ([0, 1], None),
+    ([0, 2], torch.tensor([0.25, 0.75])),
+])
+@pytest.mark.parametrize('origin', [None, True])
+@pytest.mark.parametrize('translate_back', [True, False])
+def test_centered_centroid_flow(
+        flow,
+        space_dimension,
+        subset_point_indices,
+        weights,
+        origin,
+        translate_back
+):
+    """Test with identity and MAF flow that centroid is in the expected position."""
+    batch_size = 2
+    n_points = 3
+
+    # The dimension of origin depends on space_dimension.
+    if origin is True:
+        origin = torch.ones(space_dimension)
+
+    # Create input. E.g., x is [[[0, 0, 0], [1, 1, 1], [2, 2, 2]],
+    #                           [[1, 1, 1], [2, 2, 2], [3, 3, 3]].
+    x = []
+    for batch_idx in range(batch_size):
+        x.append(np.arange(batch_idx, n_points+batch_idx, dtype=np.float64).repeat(
+            space_dimension).reshape((n_points, space_dimension)))
+    x = np.array(x)
+
+    # Compute expected centroid.
+    if subset_point_indices is None:
+        expected_centroid = np.average(x, axis=1, weights=weights)
+    else:
+        expected_centroid = np.average(x[:, subset_point_indices], axis=1, weights=weights)
+
+    # Build flow.
+    flow = CenteredCentroidFlow(
+        flow(),
+        space_dimension,
+        subset_point_indices=subset_point_indices,
+        weights=weights,
+        origin=origin,
+        translate_back=translate_back
+    )
+
+    # Run flow.
+    x = torch.tensor(atom_to_flattened(x))
+    y, _ = flow(x)
+
+    # Compute the new centroid position.
+    y = flattened_to_atom(y.detach().numpy(), space_dimension)
+    if subset_point_indices is None:
+        new_centroid = np.average(y, axis=1, weights=weights)
+    else:
+        new_centroid = np.average(y[:, subset_point_indices], axis=1, weights=weights)
+
+    # Check that centroid is correct.
+    if translate_back:
+        assert np.allclose(new_centroid, expected_centroid)
+    else:
+        if origin is None:
+            origin = np.zeros(space_dimension)
+        assert np.allclose(new_centroid, origin)
