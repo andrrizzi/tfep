755dbf8345c8769fa51bae4cb2e6276aacd47c95
Andrea Rizzi <andrrizzi@gmail.com>
2021-11-29 09:07:15 +0100

    Import PCAWhitenedFlow

--- tfep/nn/flows/__init__.py
+++ tfep/nn/flows/__init__.py
@@ -13,4 +13,5 @@ from tfep.nn.flows.centroid import CenteredCentroidFlow
 from tfep.nn.flows.maf import MAF
 from tfep.nn.flows.oriented import OrientedFlow
 from tfep.nn.flows.partial import PartialFlow
+from tfep.nn.flows.pca import PCAWhitenedFlow
 from tfep.nn.flows.sequential import SequentialFlow
