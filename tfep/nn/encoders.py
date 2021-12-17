e6dbf5ba64f3b1d7569559687d0c40ce2c7341c7
Andrea Rizzi <andrrizzi@gmail.com>
2021-12-17 18:29:26 +0100

    BehlerParrinelloRadialExpansion

--- tfep/nn/encoders.py
+++ tfep/nn/encoders.py
@@ -18,7 +18,7 @@ import torch
 
 
 # =============================================================================
-# LAYERS
+# GAUSSIAN RADIAL BASIS EXPANSION
 # =============================================================================
 
 class GaussianRadialBasisExpansion(torch.nn.Module):
@@ -29,14 +29,19 @@ class GaussianRadialBasisExpansion(torch.nn.Module):
     Schnet [1]. Note that this does not use an enveloping function that smoothly
     let this decay to 0 at a fixed cutoff.
 
+    The means and bandwidths of the Gaussians can be specified as trainable
+    parameters.
+
     Parameters
     ----------
     means : torch.Tensor
         A tensor of shape ``(n_gaussians,)`` where ``means[i]`` is the center
-        of the ``i``-th Gaussian.
+        of the ``i``-th Gaussian. The units must be the same used for the
+        input distances in ``forward()``.
     stds : torch.Tensor
         A tensor of shape ``(n_gaussians,)`` where ``stds[i]`` is the standard
-        deviation of the ``i``-th Gaussian.
+        deviation of the ``i``-th Gaussian. The units must be the same used for
+        the input distances in ``forward()``.
     trainable_means : bool, optional
         If ``True``, the means are defined as parameters of the neural network
         and optimized during training.
@@ -78,9 +83,11 @@ class GaussianRadialBasisExpansion(torch.nn.Module):
         n_gaussians : int
             The number of equidistant Gaussians.
         max_mean : float
-            The largest mean of the Gaussian.
+            The largest mean of the Gaussian in the same units used for the input
+            distances in ``forward()``.
         min_mean : float, optional
-            The smallest mean of the Gaussian.
+            The smallest mean of the Gaussian in the same units used for the
+            input distances in ``forward()``.
         relative_std : float, optional
             The standard deviation of each Gaussian relative to the displacement
             between two means. I.e., the std of the Gaussians will be set to
@@ -93,10 +100,10 @@ class GaussianRadialBasisExpansion(torch.nn.Module):
             neural network and optimized during training.
 
         """
-        spacing = (max_mean - min_mean)/(n_gaussians-1)
-        means = torch.linspace(min_mean, max_mean, n_gaussians)
-        stds = torch.full((len(means),), fill_value=relative_std*spacing)
-        return cls(means, stds, trainable_means=trainable_means, trainable_stds=trainable_stds)
+        means, stds = cls._get_equidistant_means_and_stds(
+            n_gaussians, max_mean, min_mean, relative_std)
+        return cls(means, stds, trainable_means=trainable_means,
+                   trainable_stds=trainable_stds)
 
     def forward(self, distances):
         """Expand a matrix of distances into a soft one-hot representation.
@@ -123,3 +130,140 @@ class GaussianRadialBasisExpansion(torch.nn.Module):
         gammas = self._log_gammas.exp()
         encoding = torch.exp(- gammas * disp)
         return encoding
+
+    @classmethod
+    def _get_equidistant_means_and_stds(cls, n_gaussians, max_mean, min_mean, relative_std):
+        spacing = (max_mean - min_mean)/(n_gaussians-1)
+        means = torch.linspace(min_mean, max_mean, n_gaussians)
+        stds = torch.full((len(means),), fill_value=relative_std*spacing)
+        return means, stds
+
+
+# =============================================================================
+# GAUSSIAN RADIAL BASIS EXPANSION
+# =============================================================================
+
+def behler_parrinello_cosine_switching_function(r_cutoff, r):
+    """Compute the value of the Behler-Parrinello switching function.
+
+    Parameters
+    ----------
+    r_cutoff : float
+        The cutoff imposed by the switching function. The units of ``r`` and
+        ``r_cutoff`` must be the same.
+    r : torch.Tensor
+        A tensor of shape ``(batch_size, n_atoms, n_atoms)`` where ``r[b, i, j]``
+        is the distance between atoms ``i`` and ``j`` for the ``b``-th batch.
+
+    Returns
+    -------
+    switching_value : torch.Tensor
+        A tensor of shape ``(batch_size, n_atoms, n_atoms)`` where ``switching_value[b, i, j]``
+        is the value of the switching function between atoms ``i`` and ``j`` for
+        the ``b``-th batch.
+
+    """
+    return 0.5 * torch.cos(torch.pi / r_cutoff * r) + 0.5
+
+
+class BehlerParrinelloRadialExpansion(GaussianRadialBasisExpansion):
+    """
+    Expands distance into a soft one-hot encoded vector using a Gaussian basis with a cosine switching function.
+
+    This is a Gaussian radial basis expansion multiplied by a switching function
+    similar to that used in Behler-Parrinello neural networks [1].
+
+    The means and bandwidths of the Gaussians can be specified as trainable
+    parameters.
+
+    Parameters
+    ----------
+    r_cutoff : float
+        The cutoff for the switching function in the same units used for the
+        input distances in ``forward()``.
+    means : torch.Tensor
+        A tensor of shape ``(n_gaussians,)`` where ``means[i]`` is the center
+        of the ``i``-th Gaussian. The units must be the same used for the
+        input distances in ``forward()``.
+    stds : torch.Tensor
+        A tensor of shape ``(n_gaussians,)`` where ``stds[i]`` is the standard
+        deviation of the ``i``-th Gaussian. The units must be the same used for
+        the input distances in ``forward()``.
+    trainable_means : bool, optional
+        If ``True``, the means are defined as parameters of the neural network
+        and optimized during training.
+    trainable_stds : bool, optional
+        If ``True``, the standard deviations are defined as parameters of the
+        neural network and optimized during training.
+
+    References
+    ----------
+    [1] Behler J, Parrinello M. Generalized neural-network representation of
+        high-dimensional potential-energy surfaces. Physical review letters.
+        2007 Apr 2;98(14):146401.
+
+    """
+    def __init__(self, r_cutoff, means, stds, trainable_means=False, trainable_stds=False):
+        super().__init__(means, stds, trainable_means, trainable_stds)
+        self.r_cutoff = r_cutoff
+
+    @classmethod
+    def from_range(cls, r_cutoff, n_gaussians, max_mean, min_mean=0.0, relative_std=3.0,
+                   trainable_means=False, trainable_stds=False):
+        """Create a basis of equidistant Gaussians in a given range.
+
+        By default, standard deviations are set equal to three times the
+        displacement between two consecutive gaussians.
+
+        Parameters
+        ----------
+        r_cutoff : float
+            The cutoff for the switching function in the same units used for the
+            input distances in ``forward()``.
+        n_gaussians : int
+            The number of equidistant Gaussians.
+        max_mean : float
+            The largest mean of the Gaussian in the same units used for the input
+            distances in ``forward()``.
+        min_mean : float, optional
+            The smallest mean of the Gaussian in the same units used for the
+            input distances in ``forward()``.
+        relative_std : float, optional
+            The standard deviation of each Gaussian relative to the displacement
+            between two means. I.e., the std of the Gaussians will be set to
+            ``relative_std * (means[i] - means[i-1])``.
+        trainable_means : bool, optional
+            If ``True``, the means are defined as parameters of the neural network
+            and optimized during training.
+        trainable_stds : bool, optional
+            If ``True``, the standard deviations are defined as parameters of the
+            neural network and optimized during training.
+
+        """
+        means, stds = cls._get_equidistant_means_and_stds(
+            n_gaussians, max_mean, min_mean, relative_std)
+        return cls(r_cutoff, means, stds, trainable_means=trainable_means,
+                   trainable_stds=trainable_stds)
+
+    def forward(self, distances):
+        """Expand a matrix of distances into a soft one-hot representation.
+
+        Parameters
+        ----------
+        distances : torch.Tensor
+            Distance matrix of shape ``[batch_size, n_atoms, n_atoms]`` or
+            ``[batch_size, n_atoms, n_atoms, 1]`` where ``distances[b, i, j]``
+            represent the distance between atoms ``i`` and ``j`` for the ``b``-th
+            batch.
+
+        Returns
+        -------
+        encoding : torch.Tensor
+            A matrix of shape ``[batch_size, n_atoms, n_atoms, n_gaussians]``
+            where ``n_gaussians`` is the number of Gaussian basis function used
+            to expand the distance.
+
+        """
+        encoding = super().forward(distances)
+        switching = behler_parrinello_cosine_switching_function(self.r_cutoff, distances)
+        return encoding * switching.unsqueeze(-1)
--- tfep/tests/nn/test_encoders.py
+++ tfep/tests/nn/test_encoders.py
@@ -18,7 +18,48 @@ import numpy as np
 import pytest
 import torch
 
-from tfep.nn.encoders import GaussianRadialBasisExpansion
+from tfep.nn.encoders import GaussianRadialBasisExpansion, BehlerParrinelloRadialExpansion
+
+
+# =============================================================================
+# TEST MODULE CONFIGURATION
+# =============================================================================
+
+_old_default_dtype = None
+
+def setup_module(module):
+    global _old_default_dtype
+    _old_default_dtype = torch.get_default_dtype()
+    torch.set_default_dtype(torch.double)
+
+
+def teardown_module(module):
+    torch.set_default_dtype(_old_default_dtype)
+
+
+# =============================================================================
+# UTILITY FUNCTION
+# =============================================================================
+
+def check_reference_expansion(
+        torch_impl, reference_impl, batch_size=1, n_atoms=2, n_features=1, seed=0, **kwargs):
+    """Compare the PyTorch vs the reference implementation of the expansion."""
+    generator = torch.Generator()
+    generator.manual_seed(seed)
+
+    # Create random input and measure distances.
+    x = torch.randn((batch_size, n_atoms, 3), generator=generator)
+    distances = torch.cdist(x, x)
+
+    # Create random means and stds.
+    means = torch.rand(n_features, generator=generator)
+    stds = torch.rand(n_features, generator=generator) + 0.2
+
+    # Compute torch and reference encoding.
+    encoding_pt = torch_impl(means=means, stds=stds, **kwargs)(distances)
+    encoding_np = reference_impl(distances=distances, means=means, stds=stds, **kwargs)
+
+    assert np.allclose(encoding_pt.detach().numpy(), encoding_np)
 
 
 # =============================================================================
@@ -46,6 +87,22 @@ def reference_gaussian_basis_expansion(distances, means, stds):
     return  encoding
 
 
+def reference_behler_parrinello_expansion(distances, means, stds, r_cutoff):
+    """Reference implementation of BehlerParrinelloRadialExpansion for testing."""
+    encoding = reference_gaussian_basis_expansion(distances, means, stds)
+
+    distances = distances.detach().numpy()
+
+    # Apply switching function.
+    for b in range(len(distances)):
+        for i in range(len(distances[b])):
+            for j in range(len(distances[b, i])):
+                switching = 0.5 * np.cos(torch.pi / r_cutoff * distances[b, i, j]) + 0.5
+                encoding[b, i, j] *= switching
+
+    return encoding
+
+
 # =============================================================================
 # TESTS GAUSSIAN RADIAL BASIS EXPANSION
 # =============================================================================
@@ -55,24 +112,14 @@ def reference_gaussian_basis_expansion(distances, means, stds):
 @pytest.mark.parametrize('seed', list(range(3)))
 def test_gaussian_basis_expansion_reference(batch_size, n_features, seed):
     """Compare PyTorch and reference implementation of the gaussian basis expansion."""
-    n_atoms = 5
-    generator = torch.Generator()
-    generator.manual_seed(seed)
-
-    # Create random input and measure distances.
-    x = torch.randn((batch_size, n_atoms, 3), generator=generator)
-    distances = torch.cdist(x, x)
-
-    # Create random means and stds.
-    means = torch.rand(n_features, generator=generator)
-    stds = torch.rand(n_features, generator=generator) + 0.2
-
-    # Compute torch and reference encoding.
-    expansion = GaussianRadialBasisExpansion(means, stds)
-    encoding_pt = expansion(distances)
-    encoding_np = reference_gaussian_basis_expansion(distances, means, stds)
-
-    assert np.allclose(encoding_pt.detach().numpy(), encoding_np)
+    check_reference_expansion(
+        torch_impl=GaussianRadialBasisExpansion,
+        reference_impl=reference_gaussian_basis_expansion,
+        batch_size=batch_size,
+        n_atoms=5,
+        n_features=n_features,
+        seed=seed,
+    )
 
 
 def test_gaussian_basis_expansion_equidistant():
@@ -80,3 +127,56 @@ def test_gaussian_basis_expansion_equidistant():
     expansion = GaussianRadialBasisExpansion.from_range(n_gaussians=3, max_mean=3, min_mean=1)
     assert torch.allclose(expansion._means, torch.tensor([1.0, 2.0, 3.0]))
     assert torch.allclose(expansion._log_gammas, torch.log(1./torch.full((3,), fill_value=9.0)))
+
+
+# =============================================================================
+# TESTS BEHLER-PARRINELLO RADIAL BASIS EXPANSION
+# =============================================================================
+
+@pytest.mark.parametrize('batch_size', [1, 3])
+@pytest.mark.parametrize('n_features', [1, 8])
+@pytest.mark.parametrize('seed', list(range(3)))
+def test_behler_parrinello_expansion_reference(batch_size, n_features, seed):
+    """Compare PyTorch and reference implementation of the Behler-Parrinello radial expansion."""
+    check_reference_expansion(
+        torch_impl=BehlerParrinelloRadialExpansion,
+        reference_impl=reference_behler_parrinello_expansion,
+        batch_size=batch_size,
+        n_atoms=5,
+        n_features=n_features,
+        seed=seed,
+        r_cutoff=2.0,
+    )
+
+
+@pytest.mark.parametrize('batch_size', [1, 3])
+@pytest.mark.parametrize('n_gaussians', [1, 3])
+@pytest.mark.parametrize('min_mean', [0.0, 1.0])
+def test_behler_parrinello_expansion_cutoff(batch_size, n_gaussians, min_mean):
+    """Check that value and gradient of the Behler-Parrinello expansion is 0 at the cutoff."""
+    r_cutoff = 3.0
+
+    if n_gaussians == 1:
+        expansion_layer = BehlerParrinelloRadialExpansion(
+            r_cutoff=r_cutoff,
+            means=torch.tensor([0.0]),
+            stds=torch.tensor([r_cutoff])
+        )
+    else:
+        expansion_layer = BehlerParrinelloRadialExpansion.from_range(
+            r_cutoff=r_cutoff,
+            n_gaussians=n_gaussians,
+            max_mean=r_cutoff,
+            min_mean=min_mean,
+        )
+
+    distances = torch.full((batch_size, 2, 2), fill_value=r_cutoff, requires_grad=True)
+
+    # Compute the value of the expansion at the cutoff.
+    expansion = expansion_layer(distances)
+    assert torch.allclose(expansion, torch.zeros_like(expansion))
+
+    # Compute the gradient, which should also be zero.
+    loss = torch.sum(expansion)
+    loss.backward()
+    assert torch.allclose(distances.grad, torch.zeros_like(distances))
