068bbfe3a0fd99385d4edc54f28e3260e490319c
Andrea Rizzi <andrrizzi@gmail.com>
2022-05-01 14:32:00 +0200

    utils to compute jacobian and logdetJ

--- tfep/tests/utils/test_math.py
+++ tfep/tests/utils/test_math.py
@@ -18,7 +18,7 @@ import numpy as np
 import pytest
 import torch
 
-from tfep.utils.math import cov
+from tfep.utils.math import cov, batch_autograd_jacobian
 
 
 # =============================================================================
@@ -65,3 +65,42 @@ def test_cov(ddof, dim_sample):
     cov_torch = cov(torch.tensor(x), dim_sample=dim_sample, ddof=ddof, inplace=True).numpy()
 
     assert np.allclose(cov_np, cov_torch)
+
+@pytest.mark.parametrize('in_shape,out_shape', [
+    [(), ()],
+    [(3,), (4,)],
+    [(2, 4), (2, 5)],
+])
+def test_batch_autograd_jacobian(in_shape, out_shape):
+    """Test the batch_autograd_jacobian function."""
+    batch_size = 2
+
+    # Create random input.
+    x = torch.randn(batch_size, *in_shape, requires_grad=True)
+
+    # Transformation with a linear layer.
+    in_features = 1 if len(in_shape) == 0 else in_shape[-1]
+    out_features = 1 if len(out_shape) == 0 else out_shape[-1]
+    linear = torch.nn.Linear(in_features, out_features)
+
+    def _helper(_x):
+        if len(in_shape) == 0:
+            _x = _x.unsqueeze(-1)
+        _y = linear(_x)
+        if len(out_shape) == 0:
+            _y = _y.squeeze(-1)
+        return _y
+
+    # Compute the jacobian with the batch_autograd_function.
+    y = _helper(x)
+    jac = batch_autograd_jacobian(x, y)
+
+    # Reference calculation.
+    jac_autograd_tmp = torch.autograd.functional.jacobian(_helper, x, vectorize=True)
+    jac_autograd = torch.empty(batch_size, *out_shape, *in_shape)
+    for batch_idx in range(batch_size):
+        jac_autograd[batch_idx] = jac_autograd_tmp[batch_idx].select(
+            dim=len(out_shape), index=torch.tensor(batch_idx))
+
+    assert jac.shape == (batch_size, *out_shape, *in_shape)
+    assert torch.allclose(jac, jac_autograd)
--- tfep/utils/math.py
+++ tfep/utils/math.py
@@ -133,3 +133,74 @@ def cov(x, ddof=1, dim_sample=0, inplace=False, return_mean=False):
     if return_mean:
         return c, mean
     return c
+
+
+# =============================================================================
+# CALCULUS W/ AUTOGRAD
+# =============================================================================
+
+def batch_autograd_jacobian(x, y, **grad_kwargs):
+    """Compute the batch jacobian of ``y`` w.r.t. ``x`` using ``torch.autograd.grad``.
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        Shape ``(batch_size, *)``. Input.
+    y : torch.Tensor
+        Shape ``(batch_size, *)``. Output.
+    **grad_kwargs
+        Keyword arguments for ``autograd.grad`` (e.g., ``create_graph``,
+        ``retain_graph``).
+
+    Returns
+    -------
+    jac : torch.Tensor
+        Shape ``(batch_size, size_y, size_x)``.
+
+    """
+    batch_size = x.shape[0]
+    shape_x = x.shape[1:]
+    shape_y = y.shape[1:]
+
+    # Compute the jacobian with autograd.
+    y_sum = y.sum(dim=0)
+    # Each row vector grad_outputs must have the same shape of y_sum.
+    n_features_y = torch.prod(torch.tensor(y_sum.shape), dtype=int)
+    grad_outputs = torch.eye(n_features_y).reshape(n_features_y, *y_sum.shape)
+    jacobian = torch.autograd.grad(y_sum, x, grad_outputs, is_grads_batched=True, **grad_kwargs)[0]
+
+    # From shape (n_features_y, batch_size, *x_shape) to (batch_size, n_features_y, *x_shape).
+    jacobian = jacobian.transpose(0, 1)
+
+    # Now expand dimensions y.
+    return jacobian.reshape((batch_size, *shape_y, *shape_x))
+
+
+def batch_autograd_log_abs_det_J(x, y, **grad_kwargs):
+    """Compute the batch log(abs(det(J))) of ``y`` w.r.t. ``x`` using ``torch.autograd.grad``.
+
+    Note that for the determinant to make sense, ``x`` and ``y`` must be 2D
+    tensors with identical dimensions.
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        Shape ``(batch_size, N)``. Input.
+    y : torch.Tensor
+        Shape ``(batch_size, N)``. Output.
+    **grad_kwargs
+        Keyword arguments for ``autograd.grad`` (e.g., ``create_graph``,
+        ``retain_graph``).
+
+    Returns
+    -------
+    log_det_J : torch.Tensor
+        Shape ``(batch_size,)``.
+
+    """
+    # First compute the batch jacobian.
+    jacobian = batch_autograd_jacobian(x, y, **grad_kwargs)
+
+    # Compute the log det J numerically.
+    log_det_J = torch.log(torch.abs(torch.linalg.det(jacobian)))
+    return log_det_J
