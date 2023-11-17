76f22c98d2efa9073d4c5282e2144ad0078d1fb0
Andrea Rizzi <andrrizzi@gmail.com>
2023-11-17 14:38:05 +0100

    Fix package import

--- tfep/io/__init__.py
+++ tfep/io/__init__.py
@@ -1,2 +1,6 @@
 from tfep.io.cache import TFEPCache
-from tfep.io.dataset import TrajectoryDataset, TrajectorySubset, get_subsampled_indices
+from tfep.io.dataset import (
+    DictDataset, MergedDataset, TrajectoryDataset,
+    TrajectorySubset, get_subsampled_indices,
+)
+from tfep.io.sampler import StatefulBatchSampler
