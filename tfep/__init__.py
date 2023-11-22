370394bd31c9734ccddea2bf25ec8586b2723797
Andrea Rizzi <andrrizzi@gmail.com>
2023-11-22 17:11:39 +0100

    Add __all__ in __init__.py

--- tfep/__init__.py
+++ tfep/__init__.py
@@ -1,6 +1,21 @@
+#!/usr/bin/env python
+
+
+# =============================================================================
+# PACKAGE DOCSTRING
+# =============================================================================
+
 """
-A Python library to perform targeted free energy perturbation.
+A Python library to perform targeted free energy perturbation with normalizing
+flows.
 """
+
+__all__ = ['analysis', 'app', 'io', 'nn', 'potentials', 'utils']
+
+
+# =============================================================================
+# GLOBAL IMPORTS
+# =============================================================================
 
 # versioningit
 from ._version import __version__
