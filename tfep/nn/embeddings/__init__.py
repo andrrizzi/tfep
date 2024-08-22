f41359defe51c968950699432941bac61e67574c
Andrea Rizzi <andrrizzi@gmail.com>
2024-08-22 23:16:56 +0200

    Create MAFEmbeddings

---
+++ tfep/nn/embeddings/__init__.py
@@ -0,0 +1,6 @@
+"""
+Embedding and encoder layers.
+"""
+
+from tfep.nn.embeddings.radial import GaussianBasisExpansion, BehlerParrinelloRadialExpansion
+from tfep.nn.embeddings.mafembed import MAFEmbedding, PeriodicEmbedding
