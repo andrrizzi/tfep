f68461fcb1cf8c740d81bbad85fc76b4ae1181ba
Andrea Rizzi <andrrizzi@gmail.com>
2024-09-18 18:51:11 +0200

    Make indices buffer private

--- tfep/app/mixedmaf.py
+++ tfep/app/mixedmaf.py
@@ -289,7 +289,17 @@ class MixedMAFMap(TFEPMapBase):
         # (i.e., no collinear atoms determining angles) and compute the min/max values
         # of the DOFs (after going through _CartesianToMixedFlow) to configure the
         # neural splines correctly.
-        min_dof_vals, max_dof_vals = self._analyze_dataset(z_matrix, cartesian_to_mixed_flow)
+        # TODO: REMOVE ME
+        import os
+        cache_dir_path = os.path.realpath(os.path.join(self._tfep_logger_dir_path, '..', 'cache'))
+        if os.path.isfile(os.path.join(cache_dir_path, 'cached_min_vals.pt')):
+            min_dof_vals = torch.load(os.path.join(cache_dir_path, 'cached_min_vals.pt'))
+            max_dof_vals = torch.load(os.path.join(cache_dir_path, 'cached_max_vals.pt'))
+        else:
+            min_dof_vals, max_dof_vals = self._analyze_dataset(z_matrix, cartesian_to_mixed_flow)
+            os.makedirs(cache_dir_path)
+            torch.save(min_dof_vals, os.path.join(cache_dir_path, 'cached_min_vals.pt'))
+            torch.save(max_dof_vals, os.path.join(cache_dir_path, 'cached_max_vals.pt'))
 
         # Determine the conditioning DOFs after going through _CartesianToMixedFlow.
         # conditioning_atom_indices must have the indices after the fixed atoms are removed.
